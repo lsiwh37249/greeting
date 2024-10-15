80781156d5afbbefb64f8a4e3674d39d93a8bbfa
lsiwh37249 <lsiwh37249@gmail.com>
2024-10-15 20:29:20 +0900

    li,each tag

--- src/main/java/shop/samdul/greeting/controller/greetingController.java
+++ src/main/java/shop/samdul/greeting/controller/greetingController.java
@@ -1,4 +1,8 @@
-package controller;
+package shop.samdul.greeting.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +12,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class greetingController {
 
+    Stack<String> names = new Stack<String>();
+
     @GetMapping("/greeting")
     public String greeting(
                 @RequestParam(
                     name = "name",
                     required = false,
                     defaultValue="HI") String name, Model model){
-        return "greeting";
+                names.push(name);
+                
+                model.addAttribute("names",names);
+                return "greeting";
  }
 }
